3dab2444cca323bd99069e66d3c2ddd896aa4a4a
Vijay Sharma <113002348+VijaySharma898@users.noreply.github.com>
2024-07-09 13:37:50 +0530

    init Commit

---
+++ SwiftUI-VideoList/SwiftUI-VideoList/SwiftUI_VideoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideoListApp.swift
+//  SwiftUI-VideoList
+//
+//  Created by Vijay Sharma on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
